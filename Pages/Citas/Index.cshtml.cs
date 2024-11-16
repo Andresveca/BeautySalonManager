7e3370074d24157f6012945957640d0d5d0bdcd1
Andrés Veca <carlosanvelasco@hotmail.com>
2024-11-16 14:52:23 -0500

    Pages Jordy

---
+++ Pages/Citas/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BeautySalon.Pages.Citas
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
