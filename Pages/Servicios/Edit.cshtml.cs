43c13722f176bcf0a79ffb1b486e651ea56663dd
Andrés Veca <carlosanvelasco@hotmail.com>
2024-11-21 22:34:44 -0500

    Edit Servicios

---
+++ Pages/Servicios/Edit.cshtml.cs
@@ -0,0 +1,73 @@
+using BeautySalon.Data;
+using BeautySalon.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeautySalon.Pages.Servicios
+{
+	public class EditModel : PageModel
+	{
+		private readonly SalonContext _context;
+
+		public EditModel(SalonContext context)
+		{
+			_context = context;
+		}
+
+		[BindProperty]
+		public Servicio Servicio { get; set; } = default!;
+
+		public async Task<IActionResult> OnGetAsync(int? id)
+		{
+			if (id == null || _context.Servicios == null)
+			{
+				return NotFound();
+			}
+
+			var servicio = await _context.Servicios.FirstOrDefaultAsync(m => m.Id == id);
+
+			if (servicio == null)
+			{
+				return NotFound();
+			}
+
+			Servicio = servicio;
+			return Page();
+		}
+
+		public async Task<IActionResult> OnPostAsync()
+		{
+			if (!ModelState.IsValid)
+			{
+				return Page();
+			}
+
+			_context.Attach(Servicio).State = EntityState.Modified;
+
+			try
+			{
+				await _context.SaveChangesAsync();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if (!ServicioExists(Servicio.Id))
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
+
+			return RedirectToPage("./Index");
+		}
+
+		private bool ServicioExists(int id)
+		{
+			return (_context.Servicios?.Any(e => e.Id == id)).GetValueOrDefault();
+		}
+	}
+
+}
