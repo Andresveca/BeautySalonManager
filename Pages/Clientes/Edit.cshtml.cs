25ea704cc1de0104567853e9694a7a42735f0bf6
Andrés Veca <carlosanvelasco@hotmail.com>
2024-11-21 21:47:50 -0500

    Edit Cliente

---
+++ Pages/Clientes/Edit.cshtml.cs
@@ -0,0 +1,73 @@
+using BeautySalon.Data;
+using BeautySalon.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeautySalon.Pages.Clientes
+{
+	public class EditModel : PageModel
+	{
+		private readonly SalonContext _context;
+
+		public EditModel(SalonContext context)
+		{
+			_context = context;
+		}
+
+		[BindProperty]
+		public Cliente Cliente { get; set; } = default!;
+
+		public async Task<IActionResult> OnGetAsync(int? id)
+		{
+			if (id == null || _context.Clientes == null)
+			{
+				return NotFound();
+			}
+
+			var cliente = await _context.Clientes.FirstOrDefaultAsync(m => m.Id == id);
+
+			if (cliente == null)
+			{
+				return NotFound();
+			}
+
+			Cliente = cliente;
+			return Page();
+		}
+
+		public async Task<IActionResult> OnPostAsync()
+		{
+			if (!ModelState.IsValid)
+			{
+				//return Page();
+			}
+
+			_context.Attach(Cliente).State = EntityState.Modified;
+
+			try
+			{
+				await _context.SaveChangesAsync();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if (!ClienteExists(Cliente.Id))
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
+
+			return RedirectToPage("./Index");
+		}
+
+		private bool ClienteExists(int id)
+		{
+			return (_context.Clientes?.Any(e => e.Id == id)).GetValueOrDefault();
+		}
+	}
+
+}
