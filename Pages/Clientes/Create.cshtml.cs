43e7616f79ac7c491f10104d5addf6d27499078e
Andrés Veca <carlosanvelasco@hotmail.com>
2024-11-21 21:16:31 -0500

    Create Clientes

---
+++ Pages/Clientes/Create.cshtml.cs
@@ -0,0 +1,39 @@
+using BeautySalon.Data;
+using BeautySalon.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BeautySalon.Pages.Clientes
+{
+	public class CreateModel : PageModel
+	{
+		private readonly SalonContext _context;
+
+		public CreateModel(SalonContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult OnGet()
+		{
+			return Page();
+		}
+
+		[BindProperty]
+		public Cliente Cliente { get; set; } = default!; // Cambiado a Cliente
+
+		public async Task<IActionResult> OnPostAsync()
+		{
+			if (!ModelState.IsValid || _context.Clientes == null || Cliente == null)
+			{
+				//return Page(); 
+			}
+
+			_context.Clientes.Add(Cliente); // Cambiado a Clientes
+			await _context.SaveChangesAsync();
+
+			return RedirectToPage("./Index");
+		}
+	}
+
+}
