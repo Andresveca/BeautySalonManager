73aebc4eba158a19334abf79dfcef73589d7293d
Andrés Veca <carlosanvelasco@hotmail.com>
2024-11-22 21:05:31 -0500

    Create Usuarios

---
+++ Pages/InicioSesion/Logout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BeautySalon.Pages.InicioSesion
+{
+    public class LogoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
